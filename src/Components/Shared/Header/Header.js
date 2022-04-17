a243c8901bb236f0aaf5782bdb45d5a6ba9d17a5
faysalahmed000012 <faysal000012@gmail.com>
2022-04-17 23:52:39 +0600

    responsive navbar

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -14,27 +14,33 @@ const Header = () => {
 
   return (
     <div>
-      <Navbar bg="light" variant="light">
+      <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/home">Navbar</Navbar.Brand>
-          <Nav className="ms-auto">
-            <Link className="nav-link" to="home#services">
-              Services
-            </Link>
-            <Link className="nav-link" to="/blogs">
-              Blogs
-            </Link>
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-            {user ? (
-              <button onClick={() => signOut(auth)}>Sign Out</button>
-            ) : (
-              <Link className="nav-link" to="/login">
-                Login
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link className="nav-link" to="home">
+                Home
               </Link>
-            )}
-          </Nav>
+              <Link className="nav-link" to="home#services">
+                Services
+              </Link>
+              <Link className="nav-link" to="/blogs">
+                Blogs
+              </Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+              {user ? (
+                <button onClick={() => signOut(auth)}>Sign Out</button>
+              ) : (
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
